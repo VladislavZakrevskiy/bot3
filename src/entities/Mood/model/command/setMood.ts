7934186be5308b27826b7839ce17421d32dd47eb
vlad <zakrevskiyvlada@yandex.ru>
2023-09-19 22:39:02 +0400

    bug fix

--- src/entities/Mood/model/command/setMood.ts
+++ src/entities/Mood/model/command/setMood.ts
@@ -7,9 +7,16 @@ export const configureMoodCommand = () => {
 	const bot = getBot();
 	bot.command("set_mood", async (ctx) => {
 		const user = await prisma.user.findUnique({ where: { user_id: ctx.from.id } });
+		if (!user) {
+			ctx.reply("Ошибка!");
+			return;
+		}
+
 		const keyboards = Markup.inlineKeyboard(
 			createKeyboards(
-				user?.emoji_pack.map((value) => value.toString("utf-8")) ?? ["😭", "😔", "😐", "😀", "😁"],
+				user.emoji_pack.length !== 0
+					? user.emoji_pack.map((value) => value.toString("utf-8"))
+					: ["😭", "😔", "😐", "😀", "😁"],
 				"emoji"
 			)
 		);
